7a83df8df7862e2216542a6d35a307c0a55f64c3
livemehere <rhdxoals@gmail.com>
2024-03-20 23:20:27 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://livemehere.github.io/visualize-math/",
+  base: "/visualize-math/",
   plugins: [react()],
 });
